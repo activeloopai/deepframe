2cae8e31801fd94080bae7513fa28b39fcd9bfa2
Tigran Yesayan <tigran.yesayan@activeloop.ai>
2025-02-25 15:48:14 -0500

    fix build wheel

---
+++ pyvframe/__init__.py
@@ -0,0 +1,1 @@
+from .pyvframe import *
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
 
 setup(
     name="pyvframe",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,  # Includes non-Python files like `.so`
-    package_data={
-        "pyvframe": ["*.so"],  # Ensure the shared object file is included
-    },
     install_requires=["numpy"],
+    distclass=BinaryDistribution,
+    zip_safe=False,
 )
