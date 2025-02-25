f2bdc7c25daa48d884115252268b7d5395d7cf68
Tigran Yesayan <tigran.yesayan@activeloop.ai>
2025-02-25 17:05:47 -0500

    set numpy version depending on python version

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
+import sys
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(foo):
         return True
+
+# Define numpy version based on Python version
+numpy_version = "numpy>=1.26.0,<2.0.0" if sys.version_info >= (3, 12) else "numpy>=1.24.0,<2.0.0"
 
 setup(
     name="pyvframe",
     version="0.1.0",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,  # Includes non-Python files like `.so`
-    install_requires=["numpy"],
+    install_requires=[numpy_version],
     distclass=BinaryDistribution,
     zip_safe=False,
 )
